ebf91a2bd8af4825e8b3e43f22a5dd3260ba40d3
madhuripatil06 <madhuri@thoughtworks.com>
2016-12-01 10:03:12 +0530

    tree with edges

--- javascript/tree.js
+++ javascript/tree.js
@@ -1,10 +1,28 @@
 const HEIGHT = 800;
 const WIDTH = 800;
 var data = [
-    [10, 2],
-    [9, 4],
-    [11, 4]
+    {x: 500, y: 100, name: 1},
+    {x: 450, y: 200, name: 2},
+    {x: 550, y: 200, name: 3}
 ];
+
+var lineData = [
+    {x1: 500, y1:120 , x2: 450, y2: 180  },
+    {x1: 500, y1:120 , x2: 550, y2: 180  },
+];
+
+var drawLines = function(selection){
+    var edges = selection.selectAll("line")
+        .data(lineData);
+
+    edges.enter()
+        .append("line")
+        .attr("x1", function(d){ return d.x1})
+        .attr("y1", function(d){ return d.y1})
+        .attr("x2", function(d){ return d.x2})
+        .attr("y2", function(d){ return d.y2})
+        .style("stroke", "black");
+};
 
 var drawCircles = function(x_scale, y_scale, selection){
     var tree = selection.selectAll("circle")
@@ -12,7 +30,7 @@ var drawCircles = function(x_scale, y_scale, selection){
 
     var g = tree.enter()
             .append("g")
-            .attr("transform", function(d){return "translate("+d[0]*50 +","+ d[1]*50+")"})
+            .attr("transform", function(d){return "translate("+d.x +","+ d.y+")"})
 
     g.append("circle")
         .attr("r", 20)
@@ -20,7 +38,7 @@ var drawCircles = function(x_scale, y_scale, selection){
 
     g.append("text")
         .attr("dx", function(d){return -8})
-        .text(function(d){return d[0]});
+        .text(function(d){return d.name});
 };
 
 var load = function(){
@@ -38,6 +56,7 @@ var load = function(){
         .attr("width", WIDTH);
 
     drawCircles(x_scale, y_scale, container);
+    drawLines(container);
 };
 
 window.onload = load;
